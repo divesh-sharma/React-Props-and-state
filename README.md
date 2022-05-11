39fd85de52cdb6b7de55910ee964429131dd4dfd
divesh-sharma <diveshsharma157@gmail.com>
2022-05-11 13:36:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-8bnj1t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-8bnj1t)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import ListOfCourse from './components/list-of-course.component';
+
+class App extends React.Component {
+  render() {
+    return <ListOfCourse />;
+  }
+}
+
+export default App;
